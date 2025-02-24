3f359eaab54bd38090059cdfcc69ae4bace8461d
Shubham Londhe <shubhamnath5@gmail.com>
2025-02-24 15:58:25 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,8 +14,8 @@ export function Navbar() {
         <nav className="navbar sticky-top">
             <Container className="d-flex justify-content-between align-items-center">
                 <NavLink to="/" className="navbar-brand">
-                    <img src="/imgs/logo.svg" alt="Logo" width="30" height="30" />Online
-                    <span className="text-primary text-color">Shop</span>
+                    <img src="/imgs/logo.svg" alt="Logo" width="30" height="30" />TWS DevOps
+                    <span className="text-primary text-color">Junoon</span>
                 </NavLink>
 
                 <div className="d-flex align-items-center gap-3">
