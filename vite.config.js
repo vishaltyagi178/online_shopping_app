10334524415eb394e4cd4663461f0f77a24d503b
iemafzalhassan <iemafzalhassan@gmail.com>
2025-03-07 00:47:19 +0000

    Chores: vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,19 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: './',
+    base: '/',
     css: {
       devSourcemap: false
+    },
+    server: {
+      host: true,
+      port: 5173,
+      strictPort: true,
+      allowedHosts: [
+        '16-16-128-178.nip.io',
+        'localhost'
+      ]
     }
 })
